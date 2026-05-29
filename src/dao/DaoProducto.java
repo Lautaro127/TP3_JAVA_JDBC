5df460b2109115af0347d5267fb8cba75d0c1591
unknown <lau.g01@protonmail.com>
2026-05-29 14:53:26 -0300

    commit 5

--- src/dao/DaoProducto.java
+++ src/dao/DaoProducto.java
@@ -1,5 +1,13 @@
 package dao;
 
-public class DaoProducto {
+import entidad.Producto;
 
+public class DaoProducto {
+	private String host = "jdbc:mysql://root:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String baseDeDatoString = "bdinventario";
+	
+	public DaoProducto() {}
+	
 }
