dc3fc04f908788e3fb75662cf786998e802c0f36
unknown <lau.g01@protonmail.com>
2026-05-29 12:54:40 -0300

    commit 4

--- src/entidad/Categoria.java
+++ src/entidad/Categoria.java
@@ -1,5 +1,43 @@
 package entidad;
 
 public class Categoria {
+	private int id;
+	private String nombre;
+	
+	public Categoria() {
+		id = -1;
+		nombre = "Sin nombre";
+	}
+	
+	public Categoria(int id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	public Categoria(String nombre) {
+		this.nombre = nombre;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + id + ", nombre: " + nombre + " ";
+	}
+	
+	
 }
