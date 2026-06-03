4e31b448efc195030a2d793fb502e01069bbba26
unknown <lau.g01@protonmail.com>
2026-06-03 12:37:11 -0300

    listar catergorias

--- src/dao/DaoCategoria.java
+++ src/dao/DaoCategoria.java
@@ -3,8 +3,11 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import entidad.Categoria;
+import entidad.Producto;
 
 public class DaoCategoria {
 	private String host = "jdbc:mysql://localhost:3306/";
@@ -35,5 +38,33 @@ public class DaoCategoria {
 			System.out.println("No se pudo conectar a la base de datos");
 		}
 		
+	}
+	
+	public ArrayList<Categoria> listarCategorias() {
+		ArrayList<Categoria> listaCategorias = new ArrayList<Categoria>();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		Connection conexion = null;
+		
+		try {
+			conexion = DriverManager.getConnection(host+baseDeDatoString+"?useSSL=false", user, pass);
+			query = "SELECT IdCategoria, Nombre FROM categorias";
+			PreparedStatement ps = conexion.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				Categoria categoria = new Categoria();
+				categoria.setId(rs.getInt(1));
+				categoria.setNombre(rs.getString(2));
+				
+				listaCategorias.add(categoria);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return listaCategorias;
 	}
 }
